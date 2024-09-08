78050f5b2412ac71f267486ccf959b8ed09d41cb
Parashar Das <porashorbabu@gmail.com>
2024-09-08 23:46:54 +0600

    payload test successful

--- test2.js
+++ test2.js
@@ -1,6 +1,38 @@
 const increment = "increment"
 const decrement = "decrement"
+const WithPayLoad = "withpay"
 
+
+
+
+//state init
+const count = { count : 0}
+
+
+
+
+
+//reducer function create 
+function ReDucer(state = count, action){
+    if(action.type === increment){
+        return {
+            count: state.count + 1
+        }
+    }
+    else if (action.type ===  WithPayLoad){
+        return {
+            count : state.count + action.payload
+        }
+    }
+    else{
+        return{
+            count: state.count - 1
+        }
+    }
+}
+
+
+//push function inside the dispatch 
 
 
 
@@ -14,38 +46,30 @@ function Decre(){
         type: decrement
     }
 }
+//use with the payload the function 
 
-
-//main data
-const count = { count : 0}
-
-function ReDucer(state = count, action){
-    if(action.type === increment){
-        return {
-            count: state.count + 1
-        }
-    }else{
-        return{
-            count: state.count - 1
-        }
+function WithPay(pay){
+    return{
+        type: WithPayLoad,
+        payload : pay
     }
 }
 
-
-
+//create the redux
 const {createStore} = require("redux")
 
-
+//init the store 
 const store = createStore(ReDucer)
 
-
+//subscribe the store 
 store.subscribe(()=>{
     console.log(store.getState())
 })
 
-
+//final dispatch
 store.dispatch(Incre())
 store.dispatch(Incre())
 store.dispatch(Incre())
 store.dispatch(Incre())
 store.dispatch(Incre())
+store.dispatch(WithPay(600))
